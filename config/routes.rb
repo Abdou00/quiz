148ba302b7f9ef352541af86c62f3f4574ab9470
Abdou00 <lus92390@outlook.fr>
2015-02-26 23:23:01 +0100

    Add password resets & email configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'home/index'
   get 'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
@@ -20,6 +24,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # Example resource route with options:
   #   resources :products do
   #     member do
