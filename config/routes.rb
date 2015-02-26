e503ec8b7c83b359c6ce14192329c3883d02f740
Abdou00 <lus92390@outlook.fr>
2015-02-26 19:53:15 +0100

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  get 'signup'  => 'users#new'
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -16,7 +15,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :users
 
   # Example resource route with options:
   #   resources :products do
