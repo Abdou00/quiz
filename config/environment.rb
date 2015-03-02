a851be20cc434a66108b2fb66249e7b622aeba99
Abdou00 <lus92390@outlook.fr>
2015-03-02 16:55:08 +0100

    account activation

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,24 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+require 'mail'
+
+Mail.defaults do
+  delivery_method :smtp, {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :domain => 'heroku.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+end
+
+Mail.deliver do
+  to 'from@example.com'
+  from 'sender@example.comt'
+  subject 'testing send mail'
+  body 'Welcome to the Sample App! Click on the link below to activate your account'
+end
